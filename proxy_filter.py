66955fd0413d6a59157baa5e4b61a33f456f5860
Priyansh Gupta <pg167783@gmail.com>
2022-09-24 22:56:11 +0530

    First commit

---
+++ proxy_filter.py
@@ -0,0 +1,11 @@
+links = """
+
+"""
+for BoostUpStation in links.split("\n"):
+  if(BoostUpStation[-4:] == "8080"):
+    print(BoostUpStation)
+    
+# https://proxyscrape.com/free-proxy-list
+# https://advanced.name/freeproxy
+# https://www.sslproxies.org/
+# https://premproxy.com/list/
